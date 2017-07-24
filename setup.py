fe8a918cbed6bdf04cb4317b9f4332d245ed4c98
Moshe Zadka <moshe@shopkick.com>
2017-07-24 15:06:07 -0700

    add venv freeze

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name='pexgamer',
+    version='0.0.1dev0',
+    author='Moshe Zadka',
+    author_email='zadka.moshe@gmail.com',
+    packages=setuptools.find_packages(),
+)
